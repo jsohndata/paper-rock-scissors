5748c9d5870b0837040ad6219c774b3b8db3373a
jsohnData <98851246+jsohndata@users.noreply.github.com>
2024-06-27 14:29:13 +0000

    UPDATED style and bit of script

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const updateScreen = (hideDiv, showDiv, bgColor) => () => {
 
 // Display result utility
 const displayResult = (choice) => () => {
-    resultDiv.innerHTML = `<p>You chose: ${choice}</p>`;
+    resultDiv.innerHTML = `<p>You chose</p><p class='result-text'>${choice}</p>`;
 };
 
 // Event handlers
